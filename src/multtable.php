9caaf88d360bf2f231c87d023a4709cce3f41c25
unknown <zengx@onid.oregonstate.edu>
2015-05-01 15:58:53 -0700

    fix syntax error

--- src/multtable.php
+++ src/multtable.php
@@ -10,19 +10,19 @@ ini_set('display_errors', 'On');
 	# not set
 	if(!isset($_GET['min-multiplicand'], $_GET['max-multiplicand'], $_GET['min-multiplier'], $_GET['max-multiplier']))
 	{
-		if(!isset($_GET['min-multiplicand'])
+		if(!isset($_GET['min-multiplicand']))
 		{
 			echo "Missing parameter min-multiplicand ";
 		}
-		if(!isset($_GET['max-multiplicand'])
+		if(!isset($_GET['max-multiplicand']))
 		{
 			echo "Missing parameter max-multiplicand ";
 		}
-		if(!isset($_GET['min-multiplier'])
+		if(!isset($_GET['min-multiplier']))
 		{
 			echo "Missing parameter min-multiplier ";
 		}
-			if(!isset($_GET['max-multiplier'])
+			if(!isset($_GET['max-multiplier']))
 		{
 			echo "Missing parameter max-multiplier ";
 		}
@@ -30,25 +30,25 @@ ini_set('display_errors', 'On');
 	#biger
 	else if($_GET['min-multiplicand'] > $_GET['min-multiplicand'] || $_GET['min-multiplier'] > $_GET['max-multiplier'])
 	{
-		echo "Minimum [multiplicand|multiplier] larger than maximum."
+		echo "Minimum [multiplicand|multiplier] larger than maximum.";
 
 	}
 	#if it is not a int
 	else if(!ctype_digit($_GET['min-multiplicand'], $_GET['max-multiplicand'], $_GET['min-multiplier'], $_GET['max-multiplier'])) 
 	{
-		if(!ctype_digit($_GET['min-multiplicand'])
+		if(!ctype_digit($_GET['min-multiplicand']))
 		{
 			echo "min-multiplicand must be an integer ";
 		}
-		if(!ctype_digit($_GET['max-multiplicand'])
+		if(!ctype_digit($_GET['max-multiplicand']))
 		{
 			echo "max-multiplicand must be an integer";
 		}
-		if(!ctype_digit($_GET['min-multiplier'])
+		if(!ctype_digit($_GET['min-multiplier']))
 		{
 			echo "min-multiplier must be an integer ";
 		}
-		if(!ctype_digit($_GET['max-multiplier'])
+		if(!ctype_digit($_GET['max-multiplier']))
 		{
 			echo "max-multiplier must be an integer ";
 		}
@@ -65,16 +65,16 @@ ini_set('display_errors', 'On');
 		$tall = 1;
 		$startWild = $_GET['min-multiplier'];
 		$startTall = $GET_['min-multiplicand'];
-		echo "<table>"
-			while($tall < = $maxRow)
+		echo "<table>";
+			while($tall <= $maxRow)
 			{
 				if($tall == 1)
 				{
 					echo "<tr>";
 					echo "<th>";
 					echo "<th>";
-					$wild = $ wild +1;
-					for($wild <= $maxCol)
+					$wild = $wild +1;
+					while($wild <= $maxCol)
 					{
 						echo "<th>";
 						echo $startWild;
@@ -85,27 +85,27 @@ ini_set('display_errors', 'On');
 				}
 				else
 				{
-					for($tall <=$maxRow)
+					while($tall <=$maxRow)
 					{
 						echo"<tr>";
 						echo"<th>";
 						echo $startTall;
 						echo"<th>";
-						for($wild <= $maxCol)
+						while($wild <= $maxCol)
 						{
 							echo "<th>";
-							echo $startWild * $ startTall;
+							echo $startWild * $startTall;
 							echo "<th>";
 							$startWild++;
 							$wild ++;
 						}
 						$startTall++;
+						$tall++; 
 					}				
 				}
 
 			}
-		
-		echo "</table>""
+		echo "</table>";
 	}
 	?>
 	</body>
