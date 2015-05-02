d42bfcf10d4ae7cda8d448d61e8458b0506d1944
unknown <zengx@onid.oregonstate.edu>
2015-05-01 20:20:55 -0700

    creat login file

---
+++ src/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Login Page</title>
+  </head>
+  <body>
+    <form id="loginForm" method="POST" action="content1.php">
+      <input type="text" name="username" value="Username">
+      <input type="submit" value="Login">
+    </form>
+  </body>
+</html>
